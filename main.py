4616558c36f864c33e2d8df68ef1c920eee673c0
U-MAGICSW\abhishek.singh <abhishek.singh@axoni.com>
2021-10-24 08:14:36 +0530

    Renamed my personal key to id_rsa_real and masterkey to id_rsa, now i can ssh to servers with ubuntu username

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from paramiko import SSHClient, AutoAddPolicy, client
 from rich import print, pretty, inspect
+from hosts import host_list
 
 pretty.install()
 
 client = SSHClient()
 
-hosts = ['10.0.2.148','10.0.2.248']
+# def exec_commands():
 
-for host in hosts:
-    # loading host keys
-    client.load_host_keys('c:/Users/abhishek.singh/.ssh/known_hosts')
-    client.load_system_host_keys()
+if __name__ == "__main__":
+    for host in host_list:
+        # loading host keys
+        client.load_host_keys('c:/Users/abhishek.singh/.ssh/known_hosts')
+        client.load_system_host_keys()
 
-    client.set_missing_host_key_policy(AutoAddPolicy())
+        client.set_missing_host_key_policy(AutoAddPolicy())
 
-    client.connect(host, username='magicqa')
-
-    stdin, stdout, stderr = client.exec_command('cat /etc/os-release')
-    print(f'STDOUT: {stdout.read().decode("utf8")}')
-    print(f'STDERR: {stderr.read().decode("utf8")}')
+        client.connect(host, username='ubuntu')
+        print("Following is the info for instance {}".format(host))
+        stdin, stdout, stderr = client.exec_command('cat /etc/os-release')
+        # stdin2, stdout2, stderr2 = client.exec_command('wget https://inspector-agent.amazonaws.com/linux/latest/install')
+        # stdin3, stdout3, stderr3 = client.exec_command('sudo bash install')
+        print(f'STDOUT: {stdout.read().decode("utf8")}')
+        print(f'STDERR: {stderr.read().decode("utf8")}')
+        client.close()

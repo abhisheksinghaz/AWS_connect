2a6f18337c26bfe75e9d0d3f15856b706f201f2f
U-MAGICSW\abhishek.singh <abhisheksinghaz@outlook.com>
2021-10-25 22:42:03 +0530

    just need to include the remaining instances in hosts.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from paramiko import SSHClient, AutoAddPolicy, client
 from rich import print, pretty, inspect
-from hosts import host_list
+from hosts import hosts_map
 
 pretty.install()
 
@@ -9,18 +9,17 @@ client = SSHClient()
 # def exec_commands():
 
 if __name__ == "__main__":
-    for host in host_list:
-        # loading host keys
-        client.load_host_keys('c:/Users/abhishek.singh/.ssh/known_hosts')
-        client.load_system_host_keys()
+    client.load_host_keys('c:/Users/abhishek.singh/.ssh/known_hosts')
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(AutoAddPolicy())
 
-        client.set_missing_host_key_policy(AutoAddPolicy())
+    for user,iplist in hosts_map.items():
+        for ip in iplist:
+            print(user,ip)
 
-        client.connect(host, username='ubuntu')
-        print("Following is the info for instance {}".format(host))
-        stdin, stdout, stderr = client.exec_command('cat /etc/os-release')
-        # stdin2, stdout2, stderr2 = client.exec_command('wget https://inspector-agent.amazonaws.com/linux/latest/install')
-        # stdin3, stdout3, stderr3 = client.exec_command('sudo bash install')
-        print(f'STDOUT: {stdout.read().decode("utf8")}')
-        print(f'STDERR: {stderr.read().decode("utf8")}')
-        client.close()
+            client.connect(ip, username=user)
+            print("Following is the info for instance {}".format(ip))
+            stdin, stdout, stderr = client.exec_command('cat /etc/os-release')
+            print(f'STDOUT: {stdout.read().decode("utf8")}')
+            print(f'STDERR: {stderr.read().decode("utf8")}')
+            client.close()
